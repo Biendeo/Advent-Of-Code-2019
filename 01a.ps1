8c8e1d929a671cd114dda6dba22b5f4b34b89e06
Thomas Moffet <pantherman1996@gmail.com>
2019-12-02 10:39:45 +1100

    Updated day 1 to handle arguments.

--- 01a.ps1
+++ 01a.ps1
@@ -1,7 +1,21 @@
-$Input = Get-Content (Join-Path -Path input -ChildPath 01.txt)
+param (
+	[string]$InputFile,
+	[int]$MassValue
+)
 
 function Get-FuelCount([int]$Mass) {
-	return [math]::floor($Mass / 3) - 2
+	return [Math]::Max([Math]::floor($Mass / 3) - 2, 0)
+}
+
+if (!$InputFile -and !$MassValue) {
+	Write-Output "Please specify either a mass value or a file with a bunch of masses"
+	exit
+}
+
+if ($InputFile) {
+	$Input = [int[]](Get-Content $InputFile | foreach { [int]$_ })
+} else {
+	$Input = [int[]]@($MassValue)
 }
 
 $TotalMass = 0
--- 01b.ps1
+++ 01b.ps1
@@ -1,13 +1,26 @@
-$Input = Get-Content (Join-Path -Path input -ChildPath 01.txt)
+param (
+	[string]$InputFile,
+	[int]$MassValue
+)
 
 function Get-FuelCount([Int64]$Mass) {
 	return [math]::floor($Mass / 3) - 2
 }
 
+if (!$InputFile -and !$MassValue) {
+	Write-Output "Please specify either a mass value or a file with a bunch of masses"
+	exit
+}
+
+if ($InputFile) {
+	$Input = [int[]](Get-Content $InputFile | foreach { [int]$_ })
+} else {
+	$Input = [int[]]@($MassValue)
+}
+
 $TotalMass = 0
 
 foreach ($Mass in $Input) {
-	$Mass = [int]::Parse($Mass)
 	while ($Mass -gt 6) {
 		$Mass = Get-FuelCount($Mass)
 		$TotalMass += $Mass
